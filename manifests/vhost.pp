f93e132e72d3911363d0716a46eba50825fb8100
Carmela Rubinos <carmela.rubinos@gmail.com>
2015-08-21 13:25:52 +0200

    Abstract some variables

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,16 +1,18 @@
 class midonet_mem::vhost {
 
   include ::midonet_mem::params
+  include apache
+
+  $docroot = $midonet_mem::params::mem_install_path
   $proxy_pass = [
     { 'path' => "/$midonet_mem::params::api_namespace",
       'url'  => "$midonet_mem::params::api_host"
     }
   ]
 
-  include apache
   apache::vhost { 'midonet-mem':
     port       => '80',
-    docroot    => "$midonet_mem::params::mem_install_path",
+    docroot    => $docroot,
     proxy_pass => $proxy_pass
   }
 
