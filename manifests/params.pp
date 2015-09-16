7c248126b3880bb62b506a5002521f4ca65bb2c8
Carmela Rubinos <carmela.rubinos@gmail.com>
2015-09-16 17:30:13 +0200

    Parameterized midonet_mem::vhost a bit more
    This patch adds new params for midonet_mem::vhost, as apache_port or
    docroot.

--- manifests/params.pp
+++ manifests/params.pp
@@ -19,25 +19,31 @@ class midonet_mem::params {
       $midonet_stage = 'trusty'
       $manage_distro_repo = true
       $midonet_thirdparty_repo = 'http://repo.midonet.org/misc'
-      }
+    }
 
     default: {
       fail("Unsupported platform: midonet-${module_name} only supports RedHat and Debian based OS")
     }
   }
-    $api_namespace = 'midonet-api'
-    $agent_config_api_namespace = 'conf'
-    $mem_install_path = '/var/www/html/midonet-manager'
-    $mem_config_file = "${mem_install_path}/config/client.js"
-    $keystone_token = '999888777666' # Default mocked value in midonet-api
-    $api_host = "http://${::ipaddress}:8080"
-    $login_host = "http://${::ipaddress}:8080"
-    $trace_api_host = "http://${::ipaddress}:8080"
-    $traces_ws_url = "ws://${::ipaddress}:8460"
-    $api_version = '1.9'
-    $api_token = $keystone_token # or false
-    $agent_config_api_host = "http://${::ipaddress}:8459"
-    $poll_enabled = true
 
+  # midonet_mem (init)
+  $api_namespace = 'midonet-api'
+  $agent_config_api_namespace = 'conf'
+  $mem_install_path = '/var/www/html/midonet-manager'
+  $mem_config_file = "${mem_install_path}/config/client.js"
+  $keystone_token = '999888777666' # Default mocked value in midonet-api
+  $api_host = "http://${::ipaddress}:8080"
+  $login_host = "http://${::ipaddress}:8080"
+  $trace_api_host = "http://${::ipaddress}:8080"
+  $traces_ws_url = "ws://${::ipaddress}:8460"
+  $api_version = '1.9'
+  $api_token = $keystone_token # or false
+  $agent_config_api_host = "http://${::ipaddress}:8459"
+  $poll_enabled = true
+
+  # midonet_mem::vhost
+  $apache_port = '80'
+  $servername  = "http://$::ipaddress"
+  $docroot     = '/var/www/html'
 }
 
--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,27 +1,29 @@
-class midonet_mem::vhost {
+class midonet_mem::vhost (
+  $apache_port = $midonet_mem::params::apache_port,
+  $servername  = $midonet_mem::params::servername,
+  $docroot     = $midonet_mem::params::docroot,
+  $mem_package = $midonet_mem::params::mem_package,
+  $mem_path    = $midonet_mem::params::mem_install_path,
+  $proxy_pass = [
+    { 'path' => "/$midonet_mem::params::api_namespace",
+      'url'  => "$midonet_mem::params::api_host",
+    },
+  ],
+) inherits midonet_mem::params {
 
-  include ::midonet_mem::params
   include ::apache
   include ::apache::mod::headers
 
-  $docroot = $midonet_mem::params::mem_install_path
-  $mem_package = $midonet_mem::params::mem_package
-  $proxy_pass = [
-    { 'path' => "/$midonet_mem::params::api_namespace",
-      'url'  => "$midonet_mem::params::api_host"
-    }
-  ]
-
   apache::vhost { 'midonet-mem':
-    port            => '80',
-    servername      => "http://$::ipaddress",
-    docroot         => '/var/www/html',
+    port            => $apache_port,
+    servername      => $servername,
+    docroot         => $docroot,
     proxy_pass      => $proxy_pass,
     redirect_source => '/midonet-manager',
-    redirect_dest   => $docroot,
+    redirect_dest   => $mem_path,
     directories     => [
-      { 'path'      => '/var/www/html',
-        'allow'     => 'from all',
+      { 'path'  => '/var/www/html',
+        'allow' => 'from all',
       },
     ],
     require         => Package["$mem_package"],
