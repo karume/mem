06ef18d87a419cf15cccdd6f2e12e71a1d888801
Carmela Rubinos <carmela.rubinos@gmail.com>
2015-09-17 18:14:43 +0200

    Added documentation for midonet_mem::manager
    This patch completes the already available documentation by adding info
    about how to use the class. It also provides real examples about how to
    call the class or how to override the default params values by using
    Hiera or a direct call to the class.
    
    Also the params have been classified by alphabetical sequence in groups.

--- manifests/manager.pp
+++ manifests/manager.pp
@@ -1,4 +1,4 @@
-# == Class: midonet_mem
+# == Class: midonet_mem::manager
 #
 # Install and configure Midokura Enterprise MidoNet (MEM)
 #
@@ -15,6 +15,22 @@
 #   the API host's host name and Tomcat port, or if proxied, e.g via Apache,
 #   the corresponding proxy host name and port.
 #   e.g. "api_host": "http://host:port"
+#
+# [api_version]
+#   The default value for the api_version is set to latest version. In case you
+#   are using and older MidoNet REST API, change the version accordingly.
+#   Note: The MidoNet Manager supports the following API versions: 1.8 and 1.9
+#   e.g. "api_version": "1.9"
+#
+# [api_token]
+#   If desired, auto-login can be enabled by setting the value of api_token to
+#   your Keystone token.
+#   e.g. "api_token": keystone_token
+#
+# [api_namespace]
+#   The default value for the api_namespace is set to midonet-api which usually
+#   does not have to be changed.
+#   Default value: "api_namespace": "midonet-api"
 #
 # [login_host]
 #   Configures the authentication host's host:port combination. Usually your
@@ -36,21 +52,15 @@
 #   used by the Flow Tracing feature in Midonet Manager.
 #   e.g. "trace_ws_url": "ws://host:port"
 #
-# [api_version]
-#   The default value for the api_version is set to latest version. In case you
-#   are using and older MidoNet REST API, change the version accordingly.
-#   Note: The MidoNet Manager supports the following API versions: 1.8 and 1.9
-#   e.g. "api_version": "1.9"
-#
-# [api_token]
-#   If desired, auto-login can be enabled by setting the value of api_token to
-#   your Keystone token.
-#   e.g. "api_token": keystone_token
-#
 # [agent_config_api_host]
 #   Configures the Agent Configuration API host:port combination. The Host is
 #   usually the same as the Midonet REST API and the default port is 8459.
 #   e.g. "agent_config_api_host": "http://host:port"
+#
+# [agent_config_api_namespace]
+#   The default value for the 'agent_config_api_namespace' is set to 'conf'
+#   which usually does not have to be changed.
+#   Default value: "api_namespace": "conf"
 #
 # [poll_enabled]
 #   The Auto Polling will seamlessly refresh Midonet Manager data periodically.
@@ -60,30 +70,47 @@
 #   'poll_enabled' parameter to 'false'
 #   e.g. "poll_enabled": true_or_false
 #
-# [api_namespace]
-#   The default value for the api_namespace is set to midonet-api which usually
-#   does not have to be changed.
-#   Default value: "api_namespace": "midonet-api"
-#
-# [agent_config_api_namespace]
-#   The default value for the 'agent_config_api_namespace' is set to 'conf'
-#   which usually does not have to be changed.
-#   Default value: "api_namespace": "conf"
-#
 # === Examples
 #
-# This class should be called by using 'include' if data is available in hiera:
+# This class can be called by using 'include' and it will inherits the default
+# data from 'midonet_mem::params' class or Hiera:
 #
-#   include midonet_mem
+#   include ::midonet_mem::manager
 #
-# Otherwise, it must be called by using 'class' specifying parameters and
-# values:
+# Data can be added to Hiera in order to override the default values specified
+# in 'midonet_mem::params':
 #
-# class { '::midonet_mem':
-#   mem_repo_user     => 'username',
-#   mem_repo_password => 'password',
-#   api_host          => 'http://localhost:8080',
-# }
+#   midonet_mem::manager::mem_package: 'midonet-manager'
+#   midonet_mem::manager::mem_install_path: '/var/www/html/midonet-manager'
+#   midonet_mem::manager::api_host: 'http://localhost:8080'
+#   midonet_mem::manager::api_version: '1.9'
+#   midonet_mem::manager::api_token: '999888777666'
+#   midonet_mem::manager::api_namespace: 'midonet-api'
+#   midonet_mem::manager::login_host: 'http://localhost:8080'
+#   midonet_mem::manager::trace_api_host: 'http://localhost:8080'
+#   midonet_mem::manager::traces_ws_url: 'http://localhost:8460'
+#   midonet_mem::manager::agent_config_api_host: 'http://localhost:8459'
+#   midonet_mem::manager::agent_config_api_namespace: 'conf'
+#   midonet_mem::manager::poll_enabled: true
+#
+# Otherwise, it also can be called by using 'class' specifying parameters and
+# values, which will override the default values specified in 'midonet_mem::params'
+# class and Hiera:
+#
+#   class { '::midonet_mem::manager':
+#     mem_package                => 'midonet-manager',
+#     mem_install_path           => '/var/www/html/midonet-manager',
+#     api_host                   => 'http://localhost:8080',
+#     api_version                => '1.9',
+#     api_token                  => '999888777666',
+#     api_namespace              => 'midonet-api',
+#     login_host                 => 'http://localhost:8080',
+#     trace_api_host             => 'http://localhost:8080',
+#     traces_ws_url              => 'http://localhost:8460',
+#     agent_config_api_host      => 'http://localhost:8459',
+#     agent_config_api_namespace => 'conf',
+#     poll_enabled               => true,
+#   }
 #
 # === Authors
 #
@@ -104,22 +131,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
 class midonet_mem::manager (
 # Midonet Manager installation options
   $mem_package                = $::midonet_mem::params::mem_package,
   $mem_install_path           = $::midonet_mem::params::mem_install_path,
 # Midonet Manager configuration options
   $api_host                   = $::midonet_mem::params::api_host,
+  $api_version                = $::midonet_mem::params::api_version,
+  $api_token                  = $::midonet_mem::params::api_token,
+  $api_namespace              = $::midonet_mem::params::api_namespace,
   $login_host                 = $::midonet_mem::params::login_host,
   $trace_api_host             = $::midonet_mem::params::trace_api_host,
   $traces_ws_url              = $::midonet_mem::params::traces_ws_url,
-  $api_version                = $::midonet_mem::params::api_version,
-  $api_token                  = $::midonet_mem::params::api_token,
   $agent_config_api_host      = $::midonet_mem::params::agent_config_api_host,
-  $poll_enabled               = $::midonet_mem::params::poll_enabled,
-  $api_namespace              = $::midonet_mem::params::api_namespace,
   $agent_config_api_namespace = $::midonet_mem::params::agent_config_api_namespace,
+  $poll_enabled               = $::midonet_mem::params::poll_enabled,
 ) inherits midonet_mem::params {
 
   package { 'midonet-manager':
@@ -138,4 +165,6 @@ class midonet_mem::manager (
   }
 
   include ::midonet_mem::vhost
+
 }
+
